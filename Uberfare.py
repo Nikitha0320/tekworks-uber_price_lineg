94a27a0e25911a6eb527d27d42d206d20c3cc7b6
Nikitha0320 <nikithareddygujjuri@gmail.com>
2026-03-05 12:17:52 +0530

    Update Uberfare.py

--- Uberfare.py
+++ Uberfare.py
@@ -7,7 +7,7 @@ df=pd.read_csv("Uberfare.csv")
 df.columns = df.columns.str.strip().str.lower()
 df = df.rename(columns={"distance_km": "distance"})
 
-distance=st.number_input("Distance (km)")
+distance=st.number_input("Distance (km)",min_value=0.1,max_value=50,value=0.1)
 passengers=st.number_input("Number of Passengers", min_value=1, max_value=10, value=1)
 if st.button("Predict Fare"):
     X=df[["distance","passengers"]]
@@ -21,5 +21,6 @@ if st.button("Predict Fare"):
     if passengers>1:
         fare_per_passenger=fare[0]/passengers
         st.info(f"Fare per Passenger: ₹{fare_per_passenger:.2f}")
+
 
 
